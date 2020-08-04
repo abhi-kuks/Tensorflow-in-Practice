ca826adf79708d605ec6d601b705118c32ed318d
abhi-kuks <abhishekkukreja863@gmail.com>
2020-08-04 13:45:24 +0530

    change

--- Intro_to_tensorflow/Fmnist.py
+++ Intro_to_tensorflow/Fmnist.py
@@ -2,5 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+class mycallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy')>0.7:
+            print('\n Reached 70% accuracy so cancelling training!')
+            self.model.stop_training = True
+
+callbacks = mycallback()
 fashion_mnist = keras.datasets.fashion_mnist
-(train_images , train_labels),(test_images , test_labels)= fashion_mnist.load_data()
+(x_train, y_train),(x_test, y_test) = fashion_mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = keras.models.Sequential([keras.layers.Flatten(input_shape=(28,28)),
+                                 keras.layers.Dense(512 ,activation=tf.nn.relu),
+                                 keras.layers.Dense(10 , activation=tf.nn.softmax)])
+model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy' , metrics=['accuracy'] )
+model.fit(x_train , y_train , epochs=10,callbacks=[callbacks])
+# model.predict(test_images[1])
